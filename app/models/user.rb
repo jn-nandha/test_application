b246e8e6982cb0653be9a282a73f33e1128be8ff
Jn-nandha <jigar.j.nandha@gmail.com>
2018-01-17 17:33:57 +0530

    addited merge file and commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,23 +9,24 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # Returns a random token.
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+      # Returns a random token.
+      def new_token
+        SecureRandom.urlsafe_base64
+      end
+    end
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
@@ -37,15 +38,5 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
-   # Returns the hash digest of the given string.
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # Returns a random token.
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
+  
 end 
